302ea4fc1f4287e228745a077c7da19ea369e523
Ayushman <ayushmantripathi7724@gmail.com>
2022-05-09 01:03:33 +0530

    remarks

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require('path');
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 app.use(cookieParser());
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, "client", "build")))
